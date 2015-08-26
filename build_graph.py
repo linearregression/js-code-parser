754131b4a25f9e4163c7b0f518b90d22ec793fc8
sumeet rohatgi <sum.rohatgi@gmail.com>
2015-08-25 18:45:40 -0700

    removing y.js

--- build_graph.py
+++ build_graph.py
@@ -19,13 +19,64 @@ def version(name):
         logging.info("SQLite version: {version}".format(version=data))
 
 
-def my_walker(root_dir, suffix, skip_dirs, callback):
+def find(pattern, path, skip_dirs):
+    #logging.debug("pattern: {pattern} path: {path} skip_dirs: {skip_dirs}"
+    #              .format(pattern=pattern, path=path, skip_dirs=skip_dirs))
 
-    for dirName, subdirList, fileList in os.walk(root_dir):
+    result = []
+    for root, dirs, files in os.walk(path):
+        # prune directories that need to be skipped
+        dirs[:] = [d for d in dirs if os.path.join(root, d) not in skip_dirs]
 
-        print('Found directory: %s' % dirName)
-        for fname in fileList:
-            print('\t%s' % fname)
+        for name in files:
+            full_path = os.path.join(root, name)
+            if fnmatch.fnmatch(name, pattern):
+                result.append(full_path)
+            elif full_path.endswith(pattern):
+                result.append(full_path)
+
+    return result
+
+class AssociateCode:
+
+    def __init__(self, apps, db_name):
+        self.apps = apps
+        self.db_name = db_name
+        pass
+
+        con = lite.connect(self.db_name)
+
+        with con:
+            for app in apps:
+                self.associate(con, app)
+
+    @staticmethod
+    def associate(con, app):
+        cur = con.cursor()
+        sql = "select distinct u.module " \
+              "from uses u, code c " \
+              "where c.app = '{app}' " \
+              "and u.codeid = c.codeid " \
+              "order by u.module".format(app=app['name'])
+
+        cur.execute(sql)
+
+        rows = cur.fetchall()
+
+        logging.debug("sql: {sql} fetched {num_rows} rows".format(sql=sql, num_rows=len(rows)))
+
+        cur.close()
+        cur = con.cursor()
+
+        for row in rows:
+            files = find("{module}.js".format(module=row[0]), app['shared'], app['skip'])
+            if len(files) == 1:
+                cur.execute("insert into conf(app, conffile, shortcut, filename) "
+                            "values('{app}', '{conffile}', '{shortcut}', '{filename}')"
+                            .format(app=app['name'], conffile='null', shortcut=row[0], filename=files[0]))
+                logging.debug("inserting row in conf({app},{shortcut})".format(app=app['name'], shortcut=row[0]))
+
+        cur.close()
 
 
 class UsingHeuristic:
@@ -55,7 +106,8 @@ class UsingHeuristic:
         """
 
         cur = con.cursor()
-        cur.execute("SELECT DISTINCT shortcut FROM conf f "
+        cur.execute("SELECT DISTINCT shortcut "
+                    "FROM conf f "
                     "WHERE conffile = 'null' "
                     "AND app = '{app_name}'"
                     "ORDER BY shortcut "
@@ -143,6 +195,8 @@ if __name__ == '__main__':
         appConfig = json.load(data_file)
 
     version(appConfig['db-name'])
+
+    AssociateCode(appConfig['applications'], appConfig['db-name'])
 
     heuristic = UsingHeuristic(appConfig['applications'], appConfig['db-name'])
 
