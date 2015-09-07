dab465ab3818b1a0170979568ee063ff2b378d15
sumeet rohatgi <sum.rohatgi@gmail.com>
2015-09-06 20:43:57 -0700

    removed cruft from the function

--- src/js/index/main.js
+++ src/js/index/main.js
@@ -1,18 +1,8 @@
 /* global define */
 
-define(function (require) {
-    // Load any app-specific modules
-    // with a relative require call,
-    // like:
-    var messages = require('./messages');
+define(['./messages', 'viz', 'print'], function (messages, viz, print) {
 
-    var addSankey = require('viz').addSankey;
-
-    addSankey(false, true, 'chart', "/json/viz.json");
-
-    // Load library/vendor modules using
-    // full IDs, like:
-    var print = require('print');
+    viz.addSankey(false, true, 'chart', "/json/viz.json");
 
     print(messages.getHello());
 });
