c033a60ec150d9924f393f9595b3fb8d9bbdff4f
sumeet rohatgi <sum.rohatgi@gmail.com>
2015-09-07 10:44:19 -0700

    starting backbone

--- src/js/index/main.js
+++ src/js/index/main.js
@@ -2,7 +2,52 @@
 
 /** convention: reusable components don't start with ./
  */
-define(['viz', './code'], function (viz, code) {
+define(['jquery', 'underscore', 'backbone', 'viz'], function ($, _, Backbone, viz) {
 
-    viz.addSankey(false, true, 'chart', "/json/viz.json");
+    var app = {};
+
+    app.ChartView = Backbone.View.extend({
+        el: '#chart',
+        initialize: function() {
+            viz.addSankey(false, true, 'chart', "/json/viz.json");
+        }
+    });
+
+    app.chart = new app.ChartView();
+
+    app.CodeModel = Backbone.Model.extend({
+        url: "/json/code.json"
+    });
+
+    app.codeModel = new app.CodeModel();
+
+    console.log("app.codeModel: " + app.codeModel);
+
+    app.CodeCategoryView = Backbone.View.extend({
+        el: '#code-category-headings',
+        template: _.template($("#code-category-list-tmpl").html()),
+        model: app.codeModel,
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+        }
+    });
+
+    app.codeCategoryView = new app.CodeCategoryView();
+
+    app.Router = Backbone.Router.extend({
+        routes: {
+            '*filter' : 'setFilter'
+        },
+        setFilter: function(params) {
+            console.log('app.router.params = ' + params);
+            window.filter = params && params.trim() || '';
+            app.chart.render();
+            app.codeCategoryView.render();
+        }
+    });
+
+    app.router = new app.Router();
+    Backbone.history.start();
+
+    return app;
 });
