9ae8bd33e80e9489084539b0b1c8078dee156d92
sumeet rohatgi <sum.rohatgi@gmail.com>
2015-09-08 07:14:48 -0700

    file list view is almost ready!

--- src/js/index/main.js
+++ src/js/index/main.js
@@ -16,24 +16,42 @@ define(['jquery', 'underscore', 'backbone', 'viz'], function ($, _, Backbone, vi
     app.chart = new app.ChartView();
 
     app.CodeModel = Backbone.Model.extend({
+        default: {
+            common: []
+        },
         url: "/json/code.json"
     });
-
-    app.codeModel = new app.CodeModel();
-
-    console.log("app.codeModel: " + app.codeModel);
 
     app.CodeCategoryView = Backbone.View.extend({
         el: '#code-category-headings',
         template: _.template($("#code-category-list-tmpl").html()),
+        initialize: function() {
+            this.model.on('change', this.render, this);
+        },
         render: function() {
             //{abc: 'abc', def: 'def'}
-            this.model.fetch();
-            this.$el.html(this.template(this.model.toJSON()));
+            //console.dir(this.model.toJSON());
+            this.$el.html(this.template({ hello: this.model.toJSON() }));
+            return this;
         }
     });
 
-    app.codeCategoryView = new app.CodeCategoryView({model: app.codeModel});
+    app.FileListView = Backbone.View.extend({
+        el: "#code-list",
+        template: _.template($("#code-category-file-list-tmpl").html()),
+        initialize: function() {
+            this.model.on('change', this.render, this);
+        },
+        render: function() {
+            console.log("rendering fileListView");
+            this.$el.html(this.template({ hello: this.model.toJSON() }));
+        }
+    });
+
+    app.codeModel = new app.CodeModel();
+    console.log("app.codeModel: " + app.codeModel);
+    app.codeCategoryView = new app.CodeCategoryView({ model: app.codeModel });
+    app.fileListView = new app.FileListView({ model: app.codeModel });
 
     app.Router = Backbone.Router.extend({
         routes: {
@@ -43,7 +61,7 @@ define(['jquery', 'underscore', 'backbone', 'viz'], function ($, _, Backbone, vi
             console.log('app.router.params = ' + params);
             window.filter = params && params.trim() || '';
             app.chart.render();
-            app.codeCategoryView.render();
+            app.codeModel.fetch();
         }
     });
 
